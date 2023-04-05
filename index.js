80d9d2bf0da07b32e9f3856d6862937e4fbbbe2f
Matheus <mabrisqui@gmail.com>
2023-04-05 10:00:13 -0300

    Segundo exercício resolvido

--- index.js
+++ index.js
@@ -8,8 +8,19 @@ Ryan Perez Pacheco I Rm: 98782
 
 /* 1 -Como declarar uma variável com valor undefined em JavaScript?Exemplifique e demonstre a saída do valor da variável!*/
 
-// Declarando a variável
+/* // Declarando a variável
 let qualquer = undefined
 
 // Demonstrando a saída
-console.log(qualquer)
+console.log(qualquer) */
+
+
+/* 2 -Como usar o operador % em JavaScript?Exemplifique e demonstre a saída! */
+
+// O operador de % em JavaScript significa módulo, ou seja, o retorno dessa operação é o restante da divisão de um número pelo outro
+let numero1 = parseInt(prompt("Digite um número: "))
+let numero2 = parseInt(prompt("Digite um número"))
+
+// Mostrando a saída
+conta = numero1 % numero2
+console.log(conta) 
