53caac47a5afb82aa6b7f1fc5be57b9b2c98605c
Matheus <mabrisqui@gmail.com>
2023-04-05 09:52:53 -0300

    Versão JavaScript com nome e RM dos integrantes, html com título e fonte externa e arquivo docx com as questões e capa

---
+++ index.js
@@ -0,0 +1,6 @@
+/*Guilherme Dal Posolo Matheus I Rm: 98694
+Guilherme Rezende Bezerra I Rm: 98508
+Gustavo Brisqui Martinez I Rm: 97969
+Matheus Brisqui Martinez I Rm: 97892
+Ryan Perez Pacheco I Rm: 98782
+*/
