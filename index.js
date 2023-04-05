6aa3fb5739a0037d4707360d05730d469aa2ef9c
Matheus <mabrisqui@gmail.com>
2023-04-05 10:03:24 -0300

    Terceiro exercício resolvido

--- index.js
+++ index.js
@@ -17,10 +17,30 @@ console.log(qualquer) */
 
 /* 2 -Como usar o operador % em JavaScript?Exemplifique e demonstre a saída! */
 
-// O operador de % em JavaScript significa módulo, ou seja, o retorno dessa operação é o restante da divisão de um número pelo outro
+/* // O operador de % em JavaScript significa módulo, ou seja, o retorno dessa operação é o restante da divisão de um número pelo outro
 let numero1 = parseInt(prompt("Digite um número: "))
 let numero2 = parseInt(prompt("Digite um número"))
 
 // Mostrando a saída
 conta = numero1 % numero2
-console.log(conta) 
+console.log(conta) */
+
+
+/* 3 -Escreva  um  programa  que  calcula  o  índice  de  massa  corporal  (IMC)  de  uma  pessoa  e  exibe  uma mensagem indicando se ela está abaixo, no peso ideal ou acima do peso. Considere as seguintes faixas de 
+IMC:  abaixo  de  18,5  (abaixo  do  peso),  entre  18,5  e  24,9  (peso  ideal)  e  acima  de  24,9  (acima  do  peso). Exemplifique e demonstre a saída! */
+
+// Declarando as variáveis
+let peso = parseFloat(prompt("Digite seu peso: "))
+let altura = parseFloat(prompt("Digite sua altura: "))
+
+// Fazendo os cálculos
+imc = peso / (altura * altura)
+
+// Retornando o IMC do cliente
+if(imc < 18.5){
+    document.write("Você está abaixo do peso ideal.")
+}else if(imc >= 18.5 && imc <= 24.9){
+    document.write("Você está no peso ideal.")
+}else{
+    document.write("Você está acima do peso.")
+}
