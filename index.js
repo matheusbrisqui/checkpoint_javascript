b6719d94395d8bbd3604c83671dbcbec98d295cb
Matheus <mabrisqui@gmail.com>
2023-04-05 09:57:38 -0300

    Primeiro exercício resolvido

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ Gustavo Brisqui Martinez I Rm: 97969
 Matheus Brisqui Martinez I Rm: 97892
 Ryan Perez Pacheco I Rm: 98782
 */
+
+
+/* 1 -Como declarar uma variável com valor undefined em JavaScript?Exemplifique e demonstre a saída do valor da variável!*/
+
+// Declarando a variável
+let qualquer = undefined
+
+// Demonstrando a saída
+console.log(qualquer)
