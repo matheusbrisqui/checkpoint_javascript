d5836dba23924989ef9609b6b328b1c37b1566dc
Matheus <mabrisqui@gmail.com>
2023-04-05 10:05:59 -0300

    Quarto exercício resolvido

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ console.log(conta) */
 /* 3 -Escreva  um  programa  que  calcula  o  índice  de  massa  corporal  (IMC)  de  uma  pessoa  e  exibe  uma mensagem indicando se ela está abaixo, no peso ideal ou acima do peso. Considere as seguintes faixas de 
 IMC:  abaixo  de  18,5  (abaixo  do  peso),  entre  18,5  e  24,9  (peso  ideal)  e  acima  de  24,9  (acima  do  peso). Exemplifique e demonstre a saída! */
 
-// Declarando as variáveis
+/* // Declarando as variáveis
 let peso = parseFloat(prompt("Digite seu peso: "))
 let altura = parseFloat(prompt("Digite sua altura: "))
 
@@ -43,4 +43,22 @@ if(imc < 18.5){
     document.write("Você está no peso ideal.")
 }else{
     document.write("Você está acima do peso.")
+} */
+
+
+
+/* 4 -Faça um programa que recebaa idade de uma pessoa e exibauma mensagem indicando em qual faixa etária ela se encontra: criança (até 12 anos), adolescente (entre 13 e 18 anos), adulto (entre 19 e 60 anos) ou idoso (mais de 60 anos). Exemplifique e demonstre a saída!*/
+
+// Declarando e pedindo idade da pessoa
+let idade = parseInt(prompt("Digite sua idade: "))
+
+// Dizendo ao usuário qual faixa ele se encontra
+if(idade <= 12){
+    document.write("Você é um criança.")
+}else if(idade >= 13 && idade <= 18){
+    document.write("Você é um adolescente.")
+}else if(idade >= 19 && idade <= 60){
+    document.write("Você é um adulto.")
+}else{
+    document.write("Você é um idoso.")
 }
